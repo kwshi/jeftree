c3ea5a73f006b5b895c753a9b1f27bee188591ba
Kye W. Shi <shi.kye@gmail.com>
2021-11-16 10:27:05 -0800

    start fiddling w/ codemirror

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,40 @@
 <script lang="ts">
   import * as Parser from "../lib/better-parser";
+  import * as CmView from "@codemirror/view";
+  import * as CmState from "@codemirror/state";
+  import { onMount } from "svelte";
 
   let value: string = "";
+  let container: HTMLDivElement;
 
-  $: console.log(Parser.lex(value));
+  const field = CmState.StateField.define({
+    create: () => {
+      return 0;
+    },
+    update: (value, tr) => {
+      console.log(value, tr);
+      return value;
+    },
+  });
+  const initState = CmState.EditorState.create({
+    doc: "yo",
+    extensions: field,
+  });
+
+  onMount(() => {
+    const editor = new CmView.EditorView({
+      state: initState,
+      parent: container,
+    });
+  });
 </script>
 
 <textarea bind:value />
+
+<div id="editor" bind:this={container} />
 
 <!--
 <pre>
 {JSON.stringify(Parser.parse(value), undefined, 2)}
 </pre>
--->
 -->
