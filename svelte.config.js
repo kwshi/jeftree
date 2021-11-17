2274f5ec20ed5701f418062fd9a1cc1f24e33f31
Kye Shi <shi.kye@gmail.com>
2021-11-16 17:34:38 -0800

    netlify config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
 import preprocess from "svelte-preprocess";
 
+import adapterNetlify from "@sveltejs/adapter-netlify";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess({ postcss: true }),
   compilerOptions: { dev: true },
-  kit: {},
+  kit: {
+    adapter: adapterNetlify(),
+  },
 };
 
 export default config;
